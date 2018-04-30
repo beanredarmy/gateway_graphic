17de3dd03b2182ac67bbe6cee8859806be18041c
Quan Dau <beanredarmy@gmail.com>
2018-04-30 11:22:06 +0700

    2nd  commit

--- place.h
+++ place.h
@@ -31,6 +31,11 @@ private:
     float m_curTemperature; //Nhiet do hien tai
     float m_curHumidity;    // Do am hien tai
     float m_curPressure;    // Ap suat hien tai
+
+    std::vector<float> m_hour_temp; //Mang chua du lieu nhiet do trong 1 gio (co 60 gia tri)
+    std::vector<float> m_hour_humi;
+    std::vector<float> m_hour_pres;
+
     std::vector<float> m_day_temp; //Mang chua du lieu nhiet do trong 1 ngay (co 24*60 = 1440 gia tri)
     std::vector<float> m_day_humi;
     std::vector<float> m_day_pres;
@@ -39,7 +44,9 @@ private:
     std::vector<float> m_month_humi;
     std::vector<float> m_month_pres;
 
-
+    std::vector<float> m_year_temp; // Mang chua du lieu cac nhiet do trung binh ngay trong 1 nam ( co 12 gia tri cac thang)
+    std::vector<float> m_year_humi;
+    std::vector<float> m_year_pres;
 
 
 
